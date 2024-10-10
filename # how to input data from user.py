a71d445259c4c622dc76163355452e9cf7e4469f
codeXdhruvXdeveloper <suryanshn02@gmail.com>
2024-10-10 20:44:28 +0530

    Add files via upload

---
+++ # how to input data from user.py
@@ -0,0 +1,7 @@
+# how to input data from user 
+# we use input() function always to input data from user 
+a=input("enter anything:")
+print(a,type(a))
+
+#checking the datatype of no.
+print(type(a))
