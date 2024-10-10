c0b091b60bca3a177b1dbae71ea19359ee3fec44
codeXdhruvXdeveloper <suryanshn02@gmail.com>
2024-10-10 21:00:47 +0530

    Delete # how to input integer value from user.py

--- # how to input integer value from user.py
+++
@@ -1,6 +0,0 @@
-# how to input integer value from user 
-# we use int() & input() function to get integer value from user
-a=int(input("enter a no: "))
-print(a)
-#checking that no is of int datatype or not
-print(type(a))
