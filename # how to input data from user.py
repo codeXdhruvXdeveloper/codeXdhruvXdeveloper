c2b2c45e85c775a106a6d7c4860797f481e35965
codeXdhruvXdeveloper <suryanshn02@gmail.com>
2024-10-10 21:00:06 +0530

    Delete # how to input data from user.py

--- # how to input data from user.py
+++
@@ -1,7 +0,0 @@
-# how to input data from user 
-# we use input() function always to input data from user 
-a=input("enter anything:")
-print(a,type(a))
-
-#checking the datatype of no.
-print(type(a))
