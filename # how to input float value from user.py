cc1024d27c077b7fe6f3e6c027f64abcb8d29e82
codeXdhruvXdeveloper <suryanshn02@gmail.com>
2024-10-10 21:00:37 +0530

    Delete # how to input float value from user.py

--- # how to input float value from user.py
+++
@@ -1,7 +0,0 @@
-# how to input float value from user 
-# we use float() & input() function to get integer value from user
-a=float(input("enter a no: "))
-print(a)
-
-#checking that no is of float datatype or not ]
-print(type(a))
