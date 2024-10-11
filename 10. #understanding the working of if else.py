47c1cb5896679641b374927b3aab2c1a1bcd6053
codeXdhruvXdeveloper <suryanshn02@gmail.com>
2024-10-11 21:22:27 +0530

    10. #understanding the working of if else.py

--- 10. #understanding the working of if else.py
+++ 10. #understanding the working of if else.py
@@ -2,9 +2,9 @@
 # creating a program where we find greatest no between two nos.
 a=int(input("enter 1st no:"))
 b=int(input("enter 2nd no:"))
-if(a>b):
+if(a>b): # here we are checking that a is greater than b  then print this is a greatest no 
     print(a,"is a greatest no.")
-elif(a<b):
+elif(a<b): # here we are checking that b is greater than a  then print this is a greatest no 
     print(b,"is greatest no.")
-else:
+else: # here if upper conditons not fulfill then print wrong input
     print("wrong input")
